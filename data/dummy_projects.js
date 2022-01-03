47614a355e1714bfbaa46e41126cd345742386bd
alyngu22 <alyngu22@bergen.org>
2022-01-03 14:44:05 -0500

    reformat + dummy data

---
+++ data/dummy_projects.js
@@ -0,0 +1,92 @@
+module.exports = [
+    {
+    "project_id": 1, 
+    "project_name": "EVAC",
+    "project_owners": "Alyssa, Isabel, Emma",
+    "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+    "project_link": "https://github.com/alyngu22/hackbca-example-prototypes.git",
+    "project_type": "Webapp",
+    "project_description":  "Web application with block scheduling algorithm and record book to calculate stipends."
+    },
+    {
+    "project_id": 2, 
+    "project_name": "NJCCIC",
+    "project_owners": "Sarah, Penelope, Devin",
+    "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+    "project_link": "https://github.com/penom22/capstone-njccic.git",
+    "project_type": "Webapp",
+    "project_description":  "Web application that will eat cats."
+    },
+    {
+    "project_id": 3, 
+    "project_name": "BCABus",
+    "project_owners": "John, Dayton, Clay",
+    "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+    "project_link": "https://github.com/smth/what-ever-the-name-is.git",
+    "project_type": "Mobile App",
+    "project_description":  "Mobile App that does something with buses."
+    },
+    {
+    "project_id": 4, 
+    "project_name": "Room138",
+    "project_owners": "AJ, Sierra, Mr.Respass",
+    "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+    "project_link": "https://github.com/atcs/this-is-dummy-data.git",
+    "project_type": "Web App",
+    "project_description":  "Web App that automatically feeds chickens."        
+    },
+    {
+        "project_id": 5, 
+        "project_name": "Hiccups",
+        "project_owners": "Mary, Kristina, Thomas, Greg",
+        "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+        "project_link": "https://github.com/air/i-have-really-bad-hiccups.git",
+        "project_type": "Mobile App",
+        "project_description":  "Mobile App that tracks the number of hiccips that happen before they stop."        
+    },   
+    {                   
+    "project_id": 6, 
+    "project_name": "Random",
+    "project_owners": "Kymberly, Matt, Julie",
+    "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+    "project_link": "https://github.com/sleep/is-for-the-priveleged.git",
+    "project_type": "Web App",
+    "project_description":  "Web App that can spell correctly (and also track sleep)."
+    },        
+    {                   
+        "project_id": 7, 
+        "project_name": "Hurt",
+        "project_owners": "Harold, Harriet",
+        "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+        "project_link": "https://github.com/bloop/making-dummy-data-is-boring.git",
+        "project_type": "Web App",
+        "project_description":  "Web App that will make dummy data."        
+    },    
+    {
+    "project_id": 8, 
+        "project_name": "Money",
+        "project_owners": "Jeff, Bill, Elon, Warren",
+        "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+        "project_link": "https://github.com/billionaires/have-a-lot-of-money.git",
+        "project_type": "Web App",
+        "project_description":  "Web App tracks the richest people in the world."
+    },    
+    {
+    "project_id": 9, 
+        "project_name": "Tingles",
+        "project_owners": "Alice, Brandon, Catherine, Dylan",
+        "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+        "project_link": "https://github.com/my/foot-is-falling-asleep.git",
+        "project_type": "Mobile App",
+        "project_description":  "Mobile App that sets an alarm to wake up your feet."
+    },    
+    {
+    "project_id": 10, 
+        "project_name": "Ten",
+        "project_owners": "Suess, Twain, Hemmginway",
+        "project_stack": "Javascript, HTML, CSS, Heroku, Python",
+        "project_link": "https://github.com/numten/is-this-enough.git",
+        "project_type": "Web App",
+        "project_description":  "Web App that supplies positive affirmations like: ten points of nonsense data is enough."
+    }
+]
